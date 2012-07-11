53169e18ee57d517d1851cb48c7337b162ac3967
Joel Hough <joel@joelhough.com>
2012-07-11 00:43:50 -0600

    Mucked with jabber colors to get it to inherit zenburn goodness

--- custom.el
+++ custom.el
@@ -1,6 +1,20 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(erc-nick "Snarkerd"))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (zenburn)))
+ '(erc-nick "Snarkerd")
+ '(jabber-account-list (quote (("joel@joelhough.com" (:password . "fxpxvlivfkikioqj") (:network-server . "talk.google.com") (:connection-type . ssl))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(jabber-rare-time-face ((t (:inherit (quote zenburn-green+1)))))
+ '(jabber-roster-user-away ((t (:inherit font-lock-doc))))
+ '(jabber-roster-user-chatty ((t (:inherit (quote zenburn-primary-1)))))
+ '(jabber-roster-user-dnd ((t (:inherit (quote zenburn-primary-5)))))
+ '(jabber-roster-user-error ((t (:inherit font-lock-warning))))
+ '(jabber-roster-user-offline ((t (:inherit (quote zenburn-lowlight-1)))))
+ '(jabber-roster-user-online ((t (:inherit (quote zenburn-primary-2))))))
