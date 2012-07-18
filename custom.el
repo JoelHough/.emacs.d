3d1771691982fb41d1d2ff7e14c4821354f45a04
Joel Hough <joel@joelhough.com>
2012-07-17 20:30:13 -0600

    Tweaked some jabber colors

--- custom.el
+++ custom.el
@@ -9,6 +9,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(jabber-chat-error ((t (:foreground "tomato" :weight bold))))
+ '(jabber-chat-prompt-foreign ((t (:inherit (quote zenburn-primary-2)))))
+ '(jabber-chat-prompt-local ((t (:inherit (quote zenburn-primary-1)))))
+ '(jabber-chat-prompt-system ((t (:foreground "green"))))
  '(jabber-rare-time-face ((t (:inherit (quote zenburn-green+1)))))
  '(jabber-roster-user-away ((t (:inherit font-lock-doc))))
  '(jabber-roster-user-chatty ((t (:inherit (quote zenburn-primary-1)))))
