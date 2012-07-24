3904ad260594a8c0f9244504ebd320953fd8ccc5
Joel Hough <joel@joelhough.com>
2012-07-24 15:43:20 -0600

    Remapped C-w to backwards-kill-word, C-x-C-k to kill-region.
    As suggested by https://sites.google.com/site/steveyegge2/effective-emacs

--- extra-keybindings.el
+++ extra-keybindings.el
@@ -1,3 +1,6 @@
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+
 (global-set-key (kbd "C-z") 'undo-tree-undo)
 (global-set-key [remap backward-up-list] 'backward-up-sexp)
 
