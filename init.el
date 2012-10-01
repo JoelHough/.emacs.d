e6d018b828737719bdb58dd2ac5322071c1b378d
Joel Hough <joel@instructure.com>
2012-09-30 18:28:22 -0600

    Added some mac specific settings

--- init.el
+++ init.el
@@ -4,6 +4,17 @@
 
 (add-to-list 'load-path dotfiles-dir)
 
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell 
+      (replace-regexp-in-string "[[:space:]\n]*$" "" 
+        (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(when (equal system-type 'darwin) 
+  (setq mac-command-modifier 'control)
+  (setq mac-option-modifier 'meta)
+  (set-exec-path-from-shell-PATH))
+
 (require 'ui-tweaks)
 
 ;; el-get stuff
