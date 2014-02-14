ab100498720645ba515e2919d30bcebef004898c
Joel Hough <joel@instructure.com>
2014-02-14 13:02:20 -0700

    two space indenting in css

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(css-indent-level 2)
+ '(scss-compile-at-save nil)
  '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes (quote ("d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default))))
 (custom-set-faces
