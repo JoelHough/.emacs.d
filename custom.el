44e1521b72ab6fbcfbdf17cd56a64116b32b5521
Joel Hough <joel@instructure.com>
2014-02-24 15:17:30 -0700

    ack customize tweaks

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ack-and-a-half-use-ido t)
+ '(ack-and-a-half-use-environment nil)
  '(css-indent-level 2)
  '(scss-compile-at-save nil)
  '(custom-enabled-themes (quote (zenburn)))
