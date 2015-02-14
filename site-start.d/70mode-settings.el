2d5df67ba3b3836f7f91d6dfc4300bba75884488
Joel Hough <joel@instructure.com>
2015-02-13 17:59:44 -0700

    add some auto-modes

--- site-start.d/70mode-settings.el
+++ site-start.d/70mode-settings.el
@@ -1,3 +1,4 @@
 ; Who codes in C these days? That .h is obviously a C++ .h
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
+(add-to-list 'auto-mode-alist '("\\.h$" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
