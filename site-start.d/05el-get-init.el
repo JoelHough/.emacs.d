f83bbbdf36de49df1d8fa81e8c847ed605519ee8
Joel Hough <joel@instructure.com>
2015-02-13 17:56:32 -0700

    tweak el-get installation

--- site-start.d/05el-get-init.el
+++ site-start.d/05el-get-init.el
@@ -3,10 +3,9 @@
 
 (setq el-get-user-package-directory (expand-file-name "el-get-inits/" dotfiles-dir))
 
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-   (lambda (s)
-     (let (el-get-master-branch)
-       (goto-char (point-max))
-       (eval-print-last-sexp)))))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
