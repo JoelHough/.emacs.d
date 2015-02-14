7e117a05f03b6fd7f5316ddd44a822af54ec52ee
Joel Hough <joel@instructure.com>
2015-02-13 18:01:01 -0700

    some new custom settings

--- custom.el
+++ custom.el
@@ -3,17 +3,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ack-and-a-half-use-ido t)
  '(ack-and-a-half-use-environment nil)
+ '(ack-and-a-half-use-ido t)
+ '(ag-reuse-buffers t)
+ '(c-basic-offset 2)
  '(css-indent-level 2)
- '(scss-compile-at-save nil)
  '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes (quote ("d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default))))
+ '(custom-safe-themes
+   (quote
+    ("0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default)))
+ '(scss-compile-at-save nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "apple" :family "Anonymous_Pro"))))
  '(ace-jump-face-foreground ((t (:foreground "cyan1"))))
  '(jabber-chat-error ((t (:foreground "tomato" :weight bold))) t)
  '(jabber-chat-prompt-foreign ((t (:inherit (quote zenburn-primary-2)))) t)
